5ba6c4a65f3fc45013e7a724df80c2005b1c494b
phelow <wpheloung@gmail.com>
2016-10-22 01:16:38 -0400

    Closes #2 #4 #5

--- Monster-Tinder/Assets/DialogSystem.cs
+++ Monster-Tinder/Assets/DialogSystem.cs
@@ -72,7 +72,7 @@ public class DialogSystem : MonoBehaviour {
                 bool shouldContinue = false;
                 foreach(Check c in l.m_requirements)
                 {
-                    if(PlayerPrefs.GetInt(c.m_valueName) < c.m_valueMin)
+                    if(PlayerPrefs.GetInt(c.m_valueName) <= c.m_valueMin)
                     {
                         shouldContinue = true;
                     }
@@ -160,76 +160,153 @@ public class DialogSystem : MonoBehaviour {
         m_playerChoice = choice;
     }
 
-
     public void LoadLevelOne()
     {
         Dictionary<string,DialogLine > beforeCharacterCustomizationDictionary= new Dictionary<string, DialogLine>();
 
         { //TODO: this scoping is the work of the devil, refactor this out.
             //Guess I'm doing this the old fashioned way
+            string key = "start";
             List<Check> modifiers = new List<Check>();
-            modifiers.Add(new Check("Fortitude", 1));
+            modifiers.Add(new Check(key, 1));
 
             List<DialogPair> lines = new List<DialogPair>();
-            List<Check> checks = new List<Check>();
-            checks.Add(new Check("Fortitude", 1));
-            lines.Add(new DialogPair(checks, "Hello and welcome to Monstinder. "));
 
-
-            lines.Add(new DialogPair(checks, "The elder council of monsters thanks you for doing your part to save our race."));
-
+            List<Check> checks;
+            {
+                checks = new List<Check>();
+                lines.Add(new DialogPair(checks, "Hello user. I see that you have downloaded Monstinder, the app summoned to be created by the Council of Elders as a means to combat our alarming underpopulation rates. I’m sure you must be curious as to why such a severe underpopulation crisis exists. What is your guess mortal?"));
+            }
             List<DialogPair> responses = new List<DialogPair>();
-            responses.Add(new DialogPair(checks, "Ok."));
-            responses.Add(new DialogPair(checks, "What?"));
 
-            beforeCharacterCustomizationDictionary.Add("start", new DialogLine(modifiers, lines, responses));
+            {
+                checks = new List<Check>();
+                responses.Add(new DialogPair(checks, "Just take me to the game.")); //written
+            }
+
+            {
+                checks = new List<Check>();
+                responses.Add(new DialogPair(checks, "Monsters Be Ugly..."));
+            }
+
+            {
+                checks = new List<Check>();
+                checks.Add(new Check("Idk, they're asexual?",0));
+                responses.Add(new DialogPair(checks, "Idk, they're asexual?"));
+            }
+            
+
+            beforeCharacterCustomizationDictionary.Add(key, new DialogLine(modifiers, lines, responses));
         }
 
         { //TODO: this scoping is the work of the devil, refactor this out.
             //Guess I'm doing this the old fashioned way
+            string key = "Monsters Be Ugly...";
             List<Check> modifiers = new List<Check>();
-            modifiers.Add(new Check("Incredulousness", 1));
-
+            modifiers.Add(new Check(key, 1));
+        
             List<DialogPair> lines = new List<DialogPair>();
             List<Check> checks = new List<Check>();
-            checks.Add(new Check("Fortitude", 1));
-            lines.Add(new DialogPair(checks, "Ah yes, Monstinder. The solution to the monster population crisis. Soon we will be growing and prospering again."));
+
+            lines.Add(new DialogPair(checks, "Monsters are too ugly to fall in love with one another, and because love is needed to call the Monsterodactyl to deliver the monster babies from the gods, no babies are being delivered"));
 
 
             List<Check> dialogChecks = new List<Check>();
-            checks.Add(new Check("Incredulousness", 1));
+            lines.Add(new DialogPair(dialogChecks, "Surely you must know this. "));
+
+            List<DialogPair> responses = new List<DialogPair>();
+            checks = new List<Check>();
+            responses.Add(new DialogPair(checks, "no"));
+            responses.Add(new DialogPair(checks, "Just take me to the game.."));
+
+            beforeCharacterCustomizationDictionary.Add(key, new DialogLine(modifiers, lines, responses));
+        }
+
+        { //TODO: this scoping is the work of the devil, refactor this out.
+            //Guess I'm doing this the old fashioned way
+            string key = "Idk, they're asexual?";
+            List<Check> modifiers = new List<Check>();
+            modifiers.Add(new Check(key, 1));
+            modifiers.Add(new Check("Autism", 1));
+            modifiers.Add(new Check("Intelligence", 1));
+
+            List<DialogPair> lines = new List<DialogPair>();
+            List<Check> checks = new List<Check>();
+            
+            lines.Add(new DialogPair(checks, "Um, monsters are asexual or something I don’t know! What kinda game is this anyway? This better be a good use of my time, because you gotta be efficient. Time is money. People are money. Money is money."));
+
+
+            List<Check> dialogChecks = new List<Check>();
             lines.Add(new DialogPair(dialogChecks, "Surely you must know this."));
 
             List<DialogPair> responses = new List<DialogPair>();
-            responses.Add(new DialogPair(checks, "Ok."));
-            responses.Add(new DialogPair(checks, "What?"));
+            checks = new List<Check>(); 
+            responses.Add(new DialogPair(checks, "no"));
+            responses.Add(new DialogPair(checks, "Just take me to the game.."));
 
-            beforeCharacterCustomizationDictionary.Add("What?", new DialogLine(modifiers, lines, responses));
+            beforeCharacterCustomizationDictionary.Add(key, new DialogLine(modifiers, lines, responses));
         }
 
 
         { //TODO: this scoping is the work of the devil, refactor this out.
             //Guess I'm doing this the old fashioned way
+            string key = "Fine, tell me the rules...";
             List<Check> modifiers = new List<Check>();
-            modifiers.Add(new Check("Incredulousness", 1));
 
             List<DialogPair> lines = new List<DialogPair>();
             List<Check> checks = new List<Check>();
-            checks.Add(new Check("Fortitude", 1));
-            lines.Add(new DialogPair(checks, "First we need to make a monster. On the next scene click the polaroids to make your monster. Click end to get started"));
+            lines.Add(new DialogPair(checks, ""));
 
 
             List<Check> dialogChecks = new List<Check>();
-            checks.Add(new Check("Incredulousness", 1));
-            lines.Add(new DialogPair(dialogChecks, "Its simple trust me."));
+            lines.Add(new DialogPair(dialogChecks, "Look there's going to be three screens okay. For the first click on the polaroids to make a monster. You pick the parts for making them one at a time. For the second one just click on monsters that look like you. It couldn't be simpler"));
+
+            List<DialogPair> responses = new List<DialogPair>();
+            responses.Add(new DialogPair(checks, "Monsters Be Ugly..."));
+            responses.Add(new DialogPair(checks, "Just take me to the game.."));
+
+            beforeCharacterCustomizationDictionary.Add(key, new DialogLine(modifiers, lines, responses));
+        }
+
+        { //TODO: this scoping is the work of the devil, refactor this out.
+            //Guess I'm doing this the old fashioned way
+            string key = "Just take me to the game..";
+            List<Check> modifiers = new List<Check>();
+
+            List<DialogPair> lines = new List<DialogPair>();
+            List<Check> checks = new List<Check>();
+            lines.Add(new DialogPair(checks, ""));
+
+
+            List<Check> dialogChecks = new List<Check>();
+            lines.Add(new DialogPair(dialogChecks, "Would you here the rules are?"));
+
+            List<DialogPair> responses = new List<DialogPair>();
+            responses.Add(new DialogPair(checks, "no"));
+            responses.Add(new DialogPair(checks, "Fine, tell me the rules..."));
+
+            beforeCharacterCustomizationDictionary.Add(key, new DialogLine(modifiers, lines, responses));
+        }
+
+        { //TODO: this scoping is the work of the devil, refactor this out.
+            //Guess I'm doing this the old fashioned way
+            string key = "no";
+            List<Check> modifiers = new List<Check>();
+
+            List<DialogPair> lines = new List<DialogPair>();
+            List<Check> checks = new List<Check>();
+            lines.Add(new DialogPair(checks, ""));
+
+
+            List<Check> dialogChecks = new List<Check>();
+            lines.Add(new DialogPair(dialogChecks, "Then stop wasting my time. Hit end to get started."));
 
             List<DialogPair> responses = new List<DialogPair>();
             responses.Add(new DialogPair(checks, "end"));
-            responses.Add(new DialogPair(checks, "What?"));
+            responses.Add(new DialogPair(checks, "end"));
 
-            beforeCharacterCustomizationDictionary.Add("Ok.", new DialogLine(modifiers, lines, responses));
+            beforeCharacterCustomizationDictionary.Add(key, new DialogLine(modifiers, lines, responses));
         }
-
         DialogScene beforeCharacterCustomization = new DialogScene(beforeCharacterCustomizationDictionary);
 
         m_dialogExchanges.Add(new DialogLevel(beforeCharacterCustomization, beforeCharacterCustomization, beforeCharacterCustomization, beforeCharacterCustomization, beforeCharacterCustomization));
@@ -239,15 +316,16 @@ public class DialogSystem : MonoBehaviour {
     private IEnumerator RunScene(DialogScene scene, string startingLine = "start")
     {
         m_playerChoice = "start";
-        while (m_playerChoice != "end") {
+        while (m_playerChoice != "end")
+        {
             List<string> text = scene.GetDialog(m_playerChoice);
             m_dialogText.text = text[0];
-            Fader.Instance.FadeOut(m_dialogText.gameObject);
+            Fader.Instance.FadeOut(m_dialogText.gameObject, .1f);
 
             m_choiceA.text = text[1];
-            Fader.Instance.FadeOut(m_choiceA.gameObject);
+            Fader.Instance.FadeOut(m_choiceA.gameObject, .1f);
             m_choiceB.text = text[2];
-            Fader.Instance.FadeOut(m_choiceB.gameObject);
+            Fader.Instance.FadeOut(m_choiceB.gameObject, .1f);
 
 
 
@@ -256,6 +334,10 @@ public class DialogSystem : MonoBehaviour {
             {
                 yield return new WaitForEndOfFrame();
             }
+            Fader.Instance.FadeIn(m_dialogText.gameObject,.1f);
+            Fader.Instance.FadeIn(m_choiceB.gameObject, .1f);
+            Fader.Instance.FadeIn(m_choiceA.gameObject, .1f);
+            yield return new WaitForSeconds(1.0f);
         }
 
 

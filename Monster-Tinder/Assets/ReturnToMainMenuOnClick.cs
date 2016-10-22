8be630a0e717256f9b1daac090b3f7a17c2a681d
phelow <wpheloung@gmail.com>
2016-10-22 01:41:50 -0400

    closes #7 #6 #5 #4 #3

--- Monster-Tinder/Assets/ReturnToMainMenuOnClick.cs
+++ Monster-Tinder/Assets/ReturnToMainMenuOnClick.cs
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2e11f452d2f067069e7d2691c3876b37949eb26f2b9b6d1048e6035394c2dfb4
-size 278
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ReturnToMainMenuOnClick : MonoBehaviour {
+
+    [SerializeField]
+    private UnityEngine.UI.Button m_button;
+
+	void Start(){
+
+		Fader.Instance.FadeOut (3.0f);
+	}
+
+	public void r(){
+        m_button.interactable = false;
+		Fader.Instance.FadeIn(.1f).LoadLevel( "Main Menu" );
+	}
+}
